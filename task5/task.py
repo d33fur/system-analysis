7283fc81ec99a37c1c0bf13441acb41f7e78afd7
yyxa <d33fur@gmail.com>
2024-12-26 15:00:37 +0300

    fix lab05, add lab06

--- task5/task.py
+++ task5/task.py
@@ -1,94 +1,96 @@
-import argparse
 import json
 import numpy as np
 
 
-def get_matrix(str_json: str):
-    clusters = [c if isinstance(c, list) else [c] for c in json.loads(str_json)]
+def get_matrix(filepath: str):
+    with open(filepath, 'r') as file:
+        clusters = json.load(file)
+
+    clusters = [c if isinstance(c, list) else [c] for c in clusters]
     n = sum(len(cluster) for cluster in clusters)
 
     matrix = [[1] * n for _ in range(n)]
 
+    # print(clusters)
+
     worse = []
     for cluster in clusters:
-        for worse_elem in worse:
-            for elem in cluster:
-                matrix[elem - 1][worse_elem - 1] = 0
-        for elem in cluster:
-            worse.append(int(elem))
+        for worse_element in worse:
+            for element in cluster:
+                matrix[element - 1][worse_element - 1] = 0
+                # print(element-1, worse_element-1)
+        for element in cluster:
+            worse.append(int(element))
 
     return np.array(matrix)
 
-def get_clusters(matrix, est1, est2):
-    clusters = {}
 
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-    excluded_rows = set()
-    for row in range(num_rows):
-        if row + 1 in excluded_rows:
-            continue
-        current_cluster = [row + 1]
-        clusters[row + 1] = current_cluster
-        for col in range(row + 1, num_cols):
-            if matrix[row][col] == 0:
-                current_cluster.append(col + 1)
-                excluded_rows.add(col + 1)
+def find_clusters(matrix, est1, est2):
+    conflict_core = []
 
-    result = []
-    for k in clusters:
-        if not result:
-            result.append(clusters[k])
-            continue
+    for i in range(len(matrix)):
+        for j in range(i + 1, len(matrix)):
+            if matrix[i][j] == 0 and matrix[j][i] == 0:  # Если оба элемента противоречат друг другу
+                conflict_pair = sorted([i + 1, j + 1])
+                if conflict_pair not in conflict_core:
+                    conflict_core.append(conflict_pair)
 
-        for i, elem in enumerate(result):
-            sum_est1_elem = np.sum(est1[elem[0] - 1])
-            sum_est2_elem = np.sum(est2[elem[0] - 1])
-            sum_est1_k = np.sum(est1[k - 1])
-            sum_est2_k = np.sum(est2[k - 1])
-
-            if sum_est1_elem == sum_est1_k and sum_est2_elem == sum_est2_k:
-                for c in clusters[k]:
-                    result[i].append(c)
-                    break
-            if sum_est1_elem < sum_est1_k or sum_est2_elem < sum_est2_k:
-                result = result[:i] + clusters[k] + result[i:]
-                break
-
-        result.append(clusters[k])
-
-    final_result = [r[0] if len(r) == 1 else r for r in result]
+    final_result = [pair[0] if len(pair) == 1 else pair for pair in conflict_core]
     return str(final_result)
 
+    # cluster_dict = {}
+    # rows, cols = len(matrix), len(matrix[0])
+    # processed_rows = set()
+    #
+    # for i in range(rows):
+    #     if i + 1 in processed_rows:
+    #         continue
+    #
+    #     current_cluster = [i + 1]
+    #     cluster_dict[i + 1] = current_cluster
+    #
+    #     for j in range(i + 1, cols):
+    #         if matrix[i][j] == 0:
+    #             current_cluster.append(j + 1)
+    #             processed_rows.add(j + 1)
+    #
+    # merged_clusters = []
+    # for key in cluster_dict:
+    #     if not merged_clusters:
+    #         merged_clusters.append(cluster_dict[key])
+    #         continue
+    #
+    #     for idx, cluster in enumerate(merged_clusters):
+    #         sum_est1_cluster = np.sum(est1[cluster[0] - 1])
+    #         sum_est2_cluster = np.sum(est2[cluster[0] - 1])
+    #         sum_est1_key = np.sum(est1[key - 1])
+    #         sum_est2_key = np.sum(est2[key - 1])
+    #
+    #         if sum_est1_cluster == sum_est1_key and sum_est2_cluster == sum_est2_key:
+    #             merged_clusters[idx].extend(cluster_dict[key])
+    #             break
+    #         elif sum_est1_cluster < sum_est1_key or sum_est2_cluster < sum_est2_key:
+    #             merged_clusters = merged_clusters[:idx] + cluster_dict[key] + merged_clusters[idx:]
+    #             break
+    #     else:
+    #         merged_clusters.append(cluster_dict[key])
+    #
+    # final_clusters = [cluster[0] if len(cluster) == 1 else cluster for cluster in merged_clusters]
+    # return str(final_clusters)
 
-def task(string1:str , string2:str):
-    matrix1 = get_matrix(string1)
-    matrix2 = get_matrix(string2)
 
+def main(file_path1, file_path2):
+    matrix1 = get_matrix(file_path1)
+    matrix2 = get_matrix(file_path2)
+
+    # Создаем пересечение и объединение матриц
     matrix_and = np.multiply(matrix1, matrix2)
     matrix_and_t = np.multiply(np.transpose(matrix1), np.transpose(matrix2))
-
     matrix_or = np.maximum(matrix_and, matrix_and_t)
-    clusters = get_clusters(matrix_or, matrix1, matrix2)
+
+    clusters = find_clusters(matrix_or, matrix1, matrix2)
     return clusters
 
-def find_sublists(array_str):
-    array = json.loads(array_str)
-    sublists = [element for element in array if isinstance(element, list)]
-    return sublists
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file1", type=str)
-    parser.add_argument("file2", type=str)
-    args = parser.parse_args()
-    with open(args.file1, "r") as f1:
-        string1 = f1.read().strip()
-
-    with open(args.file2, "r") as f2:
-        string2 = f2.read().strip()
-
-    clusters = task(string1, string2)
-    results = find_sublists(clusters)
-    print(results)
+if __name__ == '__main__':
+    print(main("A.json", "B.json"))
